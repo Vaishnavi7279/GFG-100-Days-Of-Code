7a7ade97d8362e039401810feb04a259b8965117
Vaishnavi7279 <srivastava93vaishnavi@gmail.com>
2023-02-10 23:46:28 +0530

    Day 1: First Commit

---
+++ Day1-Day10/1-welcome.cpp
@@ -0,0 +1,12 @@
+//hello world
+#include<iostream>
+using namespace std;
+int main()
+{
+    char name[20];
+    cout<<"Enter your name: ";
+    cin>>name;
+    cout<<"Hello "<<name<<endl;
+    cout<<"Welcome to GFG 100 Days-Of-Code!";
+    return 0;
+}
